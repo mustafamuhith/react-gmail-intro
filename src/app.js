042ba8c1c548c03ce8e9493a3c36066efff79ed4
wagnerAJS221 <wagneralex221@gmai.com>
2022-11-03 17:59:26 +0000

    error fixed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import './styles/app.css'
 import Header from './Header.js'
 import LeftMenu from './Left-menu'
+import Nav from './nav.js'
 
 import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
-import backArrow from './assets/icons/back-arrow.png'
-import downloadButton from './assets/icons/download-button.png'
 import rateStarButton from './assets/icons/rate-star-button.png'
+import backArrow from './assets/icons/back-arrow.png'
 import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
 
 function App() {
@@ -14,25 +14,7 @@ function App() {
       <Header />
       <LeftMenu />
       <main className="email-view">
-        <nav className="email-toolbar">
-          <ul>
-            <li>
-              <img className="icon" src={backArrow} alt="reply button" />
-            </li>
-            <li>
-              <img className="icon" src={downloadButton} alt="archive button" />
-            </li>
-            <li>
-              <img className="icon" src={rubbishButton} alt="delete button" />
-            </li>
-          </ul>
-          <div className="space"></div>
-          <div>
-            <p>1 of 25</p>
-            <button>&lt;</button>
-            <button>&gt;</button>
-          </div>
-        </nav>
+        <Nav />
         <article className="email-content">
           <div className="title">
             <h1>Welcome to Flaticon</h1>
