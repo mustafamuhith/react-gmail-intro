141018d62bf30ae5aa3902cf403d4d5af3a20fdc
Mustafa <mustafamuhith@gmail.com>
2022-11-03 16:13:24 +0000

    search and left-menu

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import './styles/app.css'
+import Header from './Header.js'
+import LeftMenu from './Left-menu'
 
 import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
 import backArrow from './assets/icons/back-arrow.png'
@@ -9,39 +11,8 @@ import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
 function App() {
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
-      <nav className="left-menu">
-        <ul className="inbox-list">
-          <li className="item active">
-            <span className="label">Inbox</span>
-            <span className="count">3</span>
-          </li>
-          <li className="item">
-            <span className="label">Starred</span>
-            <span className="count">2</span>
-          </li>
-
-          <li className="item toggle">
-            <label htmlFor="hide-read">Hide read</label>
-            <input id="hide-read" type="checkbox" checked={false} />
-          </li>
-        </ul>
-      </nav>
+     <Header />
+     <LeftMenu />
       <main className="email-view">
         <nav className="email-toolbar">
           <ul>
